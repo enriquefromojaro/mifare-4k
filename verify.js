2a27b1d47e01063db2b8a5f14499b4044de9e2cf
Enrique <enriquefromojaro@gmail.com>
2018-03-18 12:42:10 +0100

    Verify: Check macs and decrypt data

--- verify.js
+++ verify.js
@@ -1,14 +1,31 @@
+load('utils.js')
+function denyPay(tlvs){
+    print('Pago denegado!!')
+}
+
 function verify(payChain){
+    var card = new Card();
     payChain = new ByteString(payChain, BASE64);
     print(payChain);
 
     var tlvs = payChain.left(payChain.length -4);
     var chainMAC = payChain.right(4);
 
-    var mac = new Card().calcMAC(tlvs);
+    var mac = card.calcMAC(tlvs, card.masterKey);
     if (! mac.equals(chainMAC))
 	return denyPay(tlvs);
-    print('Patata');
+    print('Adelante!!!');
+    tlvs = Utils.bytes.decryptAES_CBC(tlvs, card.masterKey, new ByteString('00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00', HEX));
+    print(tlvs);
+   
+    chainMAC = tlvs.right(4);
+    tlvs = tlvs.left(tlvs.length -4);
+
+    mac = card.calcMAC(tlvs, card.terminalKey);
+    if (! mac.equals(chainMAC))
+	return denyPay(tlvs);
+
+    // Now we have checked the macs are correct and we have the original data
 }
 
 verify(pay(56.26));
