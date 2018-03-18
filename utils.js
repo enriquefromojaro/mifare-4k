9eb20f8b45b5968b100dd434fe881caafcb67c71
Enrique <enriquefromojaro@gmail.com>
2018-03-18 09:41:51 +0100

    Add TLV buildersto utils

--- utils.js
+++ utils.js
@@ -104,7 +104,8 @@ Card.prototype.composeCalcAndFillMAC = function(credit, emissionDate, transport,
 Utils = {
     numbers : {},
     bytes : {},
-    time: {}
+    time: {},
+    tlv: {}
 };
 
 Utils.numbers.fixedLengthIntString = function(num, length) {
@@ -223,4 +224,67 @@ Utils.time.formatDate = function(date, format) {
 	    date.getYear() % 100, 2));
 
     return cloneFormat;
+}
+
+Utils.tlv.createNameTLV = function(name){
+    var fill = new Array(30 - name.length+1).join('P');
+    return new ByteString('C1', HEX).concat(ByteString.valueOf(name.length)).concat(new ByteString(name + fill, ASCII)).left(32);
+}
+
+Utils.tlv.createcardNumberTLV = function(number){
+    // For number of 16 decimal places, we need 7 bytes
+    var numBS = ByteString.valueOf(number);
+    var fill = new Array(7-numBS.length+1).join('00');
+    
+    return new ByteString('C2 07', HEX).concat(new ByteString(fill, HEX)).concat(numBS);
+}
+
+Utils.tlv.createExpireTLV = function(date){
+    var dStr = Utils.time.formatDate(date, '%m%y');
+    return new ByteString('C3 04', HEX).concat(new ByteString(dStr, ASCII));
+}
+
+Utils.tlv.createPriceTLV = function(price){
+    var priceBS = ByteString.valueOf(price);
+    return new ByteString('C6 00', HEX).add(priceBS.length).concat(priceBS);
+}
+
+Utils.tlv.createDatetimeTLV = function(date){
+    var dateStr = Utils.time.formatDate(date, '%Y%m%d%H%M');
+    return new ByteString('E8 00', HEX).add(dateStr.length).concat(new ByteString(dateStr, ASCII));
+}
+
+Utils.tlv.createTransactionTLV = function(transType){
+    var transCodes = {
+	sell: new ByteString('00', HEX),
+	take_back: new ByteString('01', HEX),
+	reserved: new ByteString('02', HEX),
+    };
+    
+    return new ByteString('EA 01', HEX).concat(transCodes[transType]);
+}
+
+Utils.tlv.createStablismentTLV = function(stab){
+    return new ByteString('E9 00', HEX).add(stab.length).concat(new ByteString(stab, ASCII));
+}
+
+Utils.tlv.createPetitionTypeTLV = function(type){
+    var petCodes = {
+	authRequest: new ByteString('00', HEX),
+	authGaranted: new ByteString('01', HEX),
+	authDenied: new ByteString('02', HEX),
+	error: new ByteString('03', HEX),
+    };
+    
+    return new ByteString('EA 01', HEX).concat(petCodes[type]);
+}
+
+Utils.tlv.createVerificationTypeTLV = function(type){
+    var verCodes = {
+	pinVerification: new ByteString('00', HEX),
+	otherVerification: new ByteString('01', HEX),
+	reserved: new ByteString('02', HEX),
+    };
+    
+    return new ByteString('EC 01', HEX).concat(verCodes[type]);
 }
